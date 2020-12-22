ab1195b1ee8e296958de2213939e3ceb3e696e50
Adolfo Rodriguez <adolfo.rodriguez@shopify.com>
2020-12-21 22:01:09 -0800

    Fixed circumcenter calculation

--- Sources/Apollonius/Math/XYMath.swift
+++ Sources/Apollonius/Math/XYMath.swift
@@ -2,14 +2,13 @@ import Numerics
 
 extension XY {
   static func circumcenter<T: Real & Codable>(_ xy0: XY<T>, _ xy1: XY<T>, _ xy2: XY<T>) -> XY<T>? {
-    let (v0, v1, v2) = (xy0 - xy2, xy1 - xy0, xy2 - xy1)
-    let (s0, s2) = (v0.normSquared(), v2.normSquared())
-    let (d0, d1) = (v0 • v1, v1 • v2)
-    let den = 2 * (v1 >< v2).squared()
-    let (num0, num1) = (s2 * d0, s0 * d1)
-    guard let t0 = num0 / den, let t1 = num1 / den else { return nil }
-    let center0 = xy0 + (1 - t0) * (xy1 - xy0)
-    let center = center0 + (1 - t0 - t1) * (xy2 - xy1)
-    return center
+    let (v1, v2) = (xy1 - xy0, xy2 - xy0)
+    let (s1, s2) = (v1.normSquared(), v2.normSquared())
+    let denominator = 2 * (v1 >< v2)
+    let numeratorX = v2.dy.length * s1 - v1.dy.length * s2
+    let numeratorY = v1.dx.length * s2 - v2.dx.length * s1
+    guard let centerDiffX = (numeratorX / denominator)?.value, let centerDiffY = (numeratorY / denominator)?.value else { return nil }
+    let centerDiff = DXY<T>(dx: .init(value: centerDiffX), dy: .init(value: centerDiffY))
+    return xy0 + centerDiff
   }
 }
